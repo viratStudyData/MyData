dac0f7623f1463a7d92926077d6e264a839b8ddc
Emilence <emilence@Emilences-Mac-mini.local>
2018-06-22 14:16:00 +0530

    first Project

---
+++ Testing/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Testing
+//
+//  Created by Emilence on 22/06/18.
+//  Copyright © 2018 Emilence. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
